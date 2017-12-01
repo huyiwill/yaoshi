438c6647fee70941568f88ac1456bea00cb3865e
huyi@weyee.com <huyi@weyee.com>
2017-12-01 10:24:18 +0800

    asdf

--- front/script1.php
+++ front/script1.php
@@ -7,16 +7,16 @@ class againBook{
 
     public function againsend(){
         $url   = "www.twicebook.top/account/registe";
-        $str   = "来再书舒宁电器计深圳科技划深圳环时间科技舒宁电器发改委王者荣耀阿萨德飞今年可能都十分难受的是快点快点看呃呃我发哦臀大这种明年年初你的请问如同和国内出现的公婆撒啊基地散发的书法大赛吃呀看的豆腐花苏里科夫的绝哦我么么哒哦求欧盟租房安顿的";
+        $str   = "来再书舒宁电器计天天酷派王者荣耀华为手机总部苹果x赖基华深圳科技划深圳环时间科技舒宁电器发改委王者荣耀阿萨德飞今年可能都十分难受的是快点快点看呃呃我发哦臀大这种明年年初你的请问如同和国内出现的公婆撒啊基地散发的书法大赛吃呀看的豆腐花苏里科夫的绝哦我么么哒哦求欧盟租房安顿的";
         $name  = $this->getH($str);
-        $email = "abc" . time() . "@qq.com";
+        $email = substr(md5(uniqid()), 1, 8) . substr(md5(date('His')), 1, 5) . "@163.com";
         $pass  = md5('abc');
         $data  = array(
             'name'     => $name,
             'password' => $pass,
             'email'    => $email
         );
-        $res= self::curl_post_contents($url, $data, 100);
+        $res   = self::curl_post_contents($url, $data, 100);
         echo $res;
     }
 
@@ -57,25 +57,18 @@ class againBook{
     }
 
     public function getH($str){
-        // 利用preg_split函数，将汉字字符串拆分成数组，第一个参数是正则匹配，必须加上u，因为是utf8编码
-        // 这里不能使用substr或者mb_substr等，因为这些方法是针对字符有效的，汉字占2或者3个字符
         $str = preg_split('//u', $str, -1, PREG_SPLIT_NO_EMPTY);
-
-        // 利用shuffle函数，打乱汉字数组
-        // 不能使用str_shuffle函数，因为那个是打乱字符的
         shuffle($str);
-
-        // 从数组中截取前4个元素，得到的就是一个汉字数组
-        $str = array_slice($str, 0, 4);
-
-        // implode：将数组拼凑成字符串
-        $str = implode('', $str);
+        $rand = rand(1, 11);
+        $str  = array_slice($str, 0, $rand);
+        $str  = implode('', $str);
         return $str;
     }
 
 }
 
 $m = new againBook();
-for($i = 0; $i < 35; $i++){
+//$m->againsend();
+for($i = 0; $i < 5; $i++){
     $m->againsend();
 }
